60320465ed094cc740e9d9c556a43cb06944038c
Akash Nayak <113065772+ANayak66@users.noreply.github.com>
2022-09-07 17:17:59 -0400

    Update streamlit_app.py
    import pandas to read csv file and pull that data into a dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,10 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-
 streamlit.text('Avocado Toast')
-
-
 streamlit.header('Build Your Own First Smoothie')
+
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
