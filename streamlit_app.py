5de7abc7596964ab8f04b9facdc79222d8c32751
Akash Nayak <113065772+ANayak66@users.noreply.github.com>
2022-09-12 20:38:17 -0400

    Update streamlit_app.py
    switched code around

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,14 +35,14 @@ streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
-
 # take the json version of the response and normalize it 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output it the screen as a table
 streamlit.dataframe(fruityvice_normalized)
+
+# don't run anything past here while we troubleshoot
+streamlit.stop()
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','jackfruit')
 streamlit.write("Thanks for adding jackfruit")
